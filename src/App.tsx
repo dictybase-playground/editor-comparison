ee45db85fef574fda02f844ab28cbfa673158546
wildlifehexagon <crossthebreeze@gmail.com>
2021-01-07 13:34:34 -0600

    feat: center the page content

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,39 @@
 import React from "react"
 import { ApolloProvider } from "@apollo/client"
 import { BrowserRouter } from "react-router-dom"
-import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles"
+import {
+  MuiThemeProvider,
+  createMuiTheme,
+  makeStyles,
+} from "@material-ui/core/styles"
+import Grid from "@material-ui/core/Grid"
 import EditorNew from "./EditorNew"
 import useApolloClient from "./hooks/useApolloClient"
 
 const theme = createMuiTheme({})
 
+const useStyles = makeStyles({
+  container: {
+    paddingTop: "20px",
+    "& a": {
+      paddingRight: "15px",
+    },
+  },
+})
+
 const App = () => {
   const apolloClient = useApolloClient()
+  const classes = useStyles()
 
   return (
     <ApolloProvider client={apolloClient}>
       <MuiThemeProvider theme={theme}>
         <BrowserRouter>
-          <div>
-            <EditorNew />
-          </div>
+          <Grid container justify="center" className={classes.container}>
+            <Grid item>
+              <EditorNew />
+            </Grid>
+          </Grid>
         </BrowserRouter>
       </MuiThemeProvider>
     </ApolloProvider>
